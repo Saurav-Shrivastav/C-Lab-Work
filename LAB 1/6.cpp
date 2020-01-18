5bebf5f48fe62b861ca624ca715fd221dcd9c8bd
Saurav <sauravsrivastav103@gmail.com>
2020-01-19 00:59:06 +0530

    Done with this assignment

---
+++ LAB 1/6.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class Student {
+	public:
+	char Name[3][20];
+	int roll[3], marks[3];
+	int setStudentData(int i){
+		cout << "Enter the Name: ";
+		cin >> Name[i] ;
+		cout << "Enter the ROLL NO.: ";
+		cin  >> roll[i];
+		cout << "Enter the marks: ";
+		cin >> marks[i];
+	}
+	
+	int getStudentData(int i){
+		cout << "\n\nName: " << Name[i] << "\tMarks: " << marks[i] << "\tRoll No.: " << roll[i] ;
+	}
+};
+
+int main(){
+	Student S1;
+	int i;
+	for(i=0 ; i<3 ; i++){
+		S1.setStudentData(i);
+	}
+	for(i=0 ; i<3 ; i++){
+		S1.getStudentData(i);
+	}
+	
+}
